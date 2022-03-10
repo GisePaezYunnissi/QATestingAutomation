744e6d08f48eb56891ac06e5ad9856c4cbd95dc1
GisePaezYunnissi <gisesabri@hotmail.com>
2022-03-10 13:01:05 -0300

    Primera Entrega Proyecto FInal

--- src/test/java/Store.java
+++ src/test/java/Store.java
@@ -24,27 +24,18 @@ public class Store {
 
             //Busco link a categoría laptop
             driver.findElement(By.linkText("Laptops")).click();
-            //Thread.sleep(2000);
 
             //Hacer click en el primer producto
             WebDriverWait wait = new WebDriverWait(driver, 5);
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.card-img-top"))).click();
-            //driver.findElement(By.cssSelector("img.card-img-top")).click();
-            //Thread.sleep(2000);
 
             //Obtengo modelo y precio del producto, imprimo por consola
-
-            //WebDriverWait wait = new WebDriverWait(driver, 5);
-            //wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector("h2.name"))));
             modelo = driver.findElement(By.cssSelector("h2.name")).getText();
             precio = driver.findElement(By.cssSelector("h3.price-container")).getText();
             System.out.println("Modelo: " + modelo + "Precio: " + precio);
 
             //Agrego el producto al cart
             driver.findElement(By.linkText("Add to cart")).click();
-            //Thread.sleep(2000);
-
-            //WebDriverWait wait = new WebDriverWait(driver, 5);
             wait.until(ExpectedConditions.alertIsPresent());
             driver.switchTo().alert().accept();
             driver.quit();
